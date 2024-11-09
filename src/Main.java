72eb578f4f5d9e4aa3bda110168304a05dd94244
Chvost <divadelnimail@gmail.com>
2024-11-09 19:51:38 +0100

    toto

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
+
+
+
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] names = {"Karel", "Pepa"};
+        String[] names = {" ", " ", " ", " "};
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadej číslo:");
         int a = sc.nextInt();
 
         if (a == 1) {
-            names[1] = "X|";
-        }
+            names[0] = "X";
 
-        System.out.println(names[1]);
+
+        } if (a == 2) {
+                names[3] = "X";
+
+            }
+        System.out.println(names[0] + "|" + names[1] + "|" + "\n-----" + "\n" + names[2] + "|" + names[3] + "|");
+
+
+
+
     }
 }
+
+
+
+
+
